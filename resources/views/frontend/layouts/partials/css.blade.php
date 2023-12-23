1e80787b0fb3c48ab677fcbf5baca7e6b34424bc
tanim <Sadrul445@users.noreply.github.com>
2023-12-23 16:06:01 +0600

    Added Assets of Website Frontend

---
+++ resources/views/frontend/layouts/partials/css.blade.php
@@ -0,0 +1,9 @@
+@stack('css')
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/ui/frontend/css/bootstrap.min.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/ui/frontend/css/elegant-icons.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/ui/frontend/css/font-awesome.min.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/ui/frontend/css/jquery-ui.min.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/ui/frontend/css/nice-select.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/ui/frontend/css/owl.carousel.min.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/ui/frontend/css/slicknav.min.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/ui/frontend/css/style.css') }}">
